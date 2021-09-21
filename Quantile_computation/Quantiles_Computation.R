4bd532eaacc8d07f1457e41e88066fb14387a2a8
Craig Slinkman <cwslinkman@gmail.com>
2021-09-20 20:14:54 -0500

    2021-09-20 20:14

--- Quantile_computation/Quantiles_Computation.R
+++ Quantile_computation/Quantiles_Computation.R
@@ -1,8 +1,8 @@
-# Quantile Computations
+# R_quartile_computations
 
 ################################################################################
-# This cript shows how to use the R quantile function to computer the quartiles
-# of a distribution.  We wil use the data set USArrests which is p[rovided by
+# This script shows how to use the R quantile function to computer the quartiles
+# of a distribution.  We will use the data set USArrests which is p[rovided by
 #  R.
 #
 # This data set contains statistics, in arrests per 100,000 residents for 
@@ -10,36 +10,63 @@
 # the percent of the population living in urban areas.
 ################################################################################
 
-head( USArrests )                       # Display first 10 rows of  the data.
+data( USArrests )                      # Load data sets.
+head( USArrests )                   # Display first 10 rows of  the data.
 
 ################################################################################
 # We are asked to compute the quantile values for Q1, Q2, Q3, and Q4. In the
 # below I will list the corresponding precentile and quantil for each quartile
-# listed abive.  I will   We will use US Arrests for murders for our data.
+# listed ab0ve.  I will   We will use US Arrests for murders for our data.
 # 
-# Quartile   Percentile   fraction
+# Quartile   Percentile   quantile
 
 # Q1                25%       0.25
 # Q2                50%       0.50
 # Q3                75%       0.75
 # Q4               100%       1.00
 
-Q1 <- quantile(USArrests$Murder, 0.25 )
-Q1
-Q2 <- quantile(USArrests$Murder, 0.50 )
-Q2
-Q3 <- quantile(USArrests$Murder, 0.75 )
-Q3
-Q4 <- quantile(USArrests$Murder, 1.00 )
-Q4
 
 ################################################################################
 # We can also do the same operatrion by using a vector of the frqactions.
 ################################################################################
 
-Q <- quantile(USArrests$Murder, c(0.25,0.50,0.75,1.00))
-Q
+quantile(USArrests$Murder)
 
 ################################################################################
-# There is even a more parsimonius way to compute thequartiles.
+# Complementing the  quartile computations with graphs.  We will draw a
+# density histogram and a boxplot.  I prefer to use the cowplot them for my
+# plots.  You will need to install this package if you do not use it.
 ################################################################################
+
+require(tidyverse)                      # I live in the tidyverse.
+require(cowplot)                        # For professional plots.
+
+ggplot( USArrests,
+        aes( x = Murder, y = ..density.. )) +
+    geom_histogram( bins = 5,
+                    fill = "green",
+                    color="black") +
+    geom_density() +
+    xlab( "Murders per 100,000 population") +
+    ylab( "Density" ) +
+    ggtitle( "US Murders per 100,000 in 1973") +
+    theme_cowplot()
+
+
+x_breaks <- 
+    seq( from = 0, by = 1, to = 20 )
+
+ggplot( USArrests,
+        aes(x = Murder, y = NULL )) +
+    geom_boxplot( fill = "green", size = 1) +
+    xlab( "Mirders per 100,000")  +
+    ylab( "" ) +
+    scale_x_continuous( breaks = x_breaks,
+                        labels = x_breaks ) +
+    ggtitle( "US Murders per 100,000 in 1973") +
+    theme_cowplot() +
+    theme( axis.line.y = element_blank(),
+           axis.text.y = element_blank(),
+           axis.ticks.y = element_blank())
+
+
